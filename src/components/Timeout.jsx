cd9cab9e20ee184e1e0e4e1ab5b94af6c0b9db71
vrramosm <vf.ramos7@gmail.com>
2020-01-23 16:50:44 -0300

    Finalizado o componente TimeOut

---
+++ src/components/Timeout.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCount: 30,
+      getCurrentTime: 0,
+      isPaused: true,
+    }
+    this.timer = this.timer.bind(this);
+    this.getTimeOut = this.getTimeOut.bind(this);
+  }
+
+  timer() {
+    if (this.state.isPaused) {
+      this.setState({
+        currentCount: this.state.currentCount - 1
+      })
+    }
+    if (this.state.currentCount < 1) {
+      clearInterval(this.intervalId);
+    }
+  }
+  
+  getTimeOut() {
+    this.setState({
+      isPaused: false,
+      getCurrentTime: this.state.currentCount
+    });
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(this.timer, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.currentCount}</p>
+        <button button="button" onClick={() => this.getTimeOut()}>Get Time</button>
+      </div>
+    );
+  }
+}
+
+export default Clock;
