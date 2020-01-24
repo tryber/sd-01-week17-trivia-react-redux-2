a5afc70a345284af660659ad06fa1ed3c4b8c9b5
Guilherme Luiz <guiiluiz44@gmail.com>
2020-01-24 18:05:49 -0300

    organize files and folders

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../style/Ranking.css';
+import { Link } from 'react-router-dom';
 
 function Ranking() {
   const ranking = JSON.parse(localStorage.getItem('ranking'));
   const playersScore = ranking.map((player) => player.score);
   const scoreRank = playersScore.sort((a, b) => b - a);
   const playersRank = scoreRank.map((score) => ranking.find((player) => player.score === score));
-  const leaderboard = playersRank.map((player) => (
+  const leaderboard = playersRank.map((player, position) => (
     <div className="ranking-player">
-      <img src={player.picture} alt="Game Player" />
-      <h4>{player.name}</h4>-
+      <img data-testid={`profile-picture-${position}`} src={player.picture} alt="Game Player" />
+      <h4 data-testid={`${player.name}-${position}`}>{player.name}</h4>-
       <p>{player.score} Points</p>
     </div>
   ));
@@ -18,6 +19,9 @@ function Ranking() {
     <div className="ranking-content">
       <h1 className="title">Ranking</h1>
       {leaderboard}
+      <Link to="/feedback">
+        <button type="button">Return</button>
+      </Link>
     </div>
   );
 }
