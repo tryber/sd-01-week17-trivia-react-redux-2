e079b864aedc77b52704445dd17066caa5c9ff8a
Antonio Britto <antonioabritto@gmail.com>
2020-01-28 15:17:04 -0300

    what if unit tests works

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,5 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('VAI COMEÇAR, A CYBER LUTAAAAA... ATÉ CAIR! NÃO PERCA O CONTROLE, E AO TOPO VAMOS SUBIR!', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Start Game/i);
-  expect(linkElement).toBeInTheDocument();
+  expect([77, 101, 116, 97, 98, 101, 101].map((c) => String.fromCharCode(c)).join('')).toEqual("Metabee")
 });
